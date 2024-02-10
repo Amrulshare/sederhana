26cf1534947b679b597ba5e3b2c8fcb99f7bc519
Amrulshare <amrulfile@gmail.com>
2024-02-10 15:05:09 +0700

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { HashLoader } from "react-spinners";
+import Home from "./pages/Home";
+
 function App() {
+  const [loader, setLoader] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    setLoader(true);
+    setTimeout(() => {
+      setLoader(false);
+    }, 5000);
+  }, []);
   return (
     <>
-      <div></div>
+      {loader ? (
+        <div id="loader">
+          <HashLoader size={130} color="#36d7b7" speedMultiplier={2} />
+        </div>
+      ) : (
+        <Routes>
+          <Route path="" element={<Home />} />
+        </Routes>
+      )}
     </>
   );
 }
